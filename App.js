ef7d2211a73d40cdf4c7ac6be907f7f35d24bc0b
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-19 11:54:55 +0900

    added react navigation native/stack

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Home from './screens/Home';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <Home />
-  );
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ screens/MeeetingRoom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function MeeetingRoom() {
+  return (
+    <View>
+      <Text>Meeting Room</Text>
+    </View>
+  );
+}
+
+export default MeeetingRoom;
