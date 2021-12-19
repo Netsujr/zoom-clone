59ee577d459c18358cae37056a23c41ac561efd3
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-19 11:04:01 +0900

    array not looping

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import AntDesign from "react-native-vector-icons/AntDesign";
 
+const contactsMenuButtons = [
+  {
+    type: "starred"
+  },
+  {
+    type: 'contact',
+    name: "Renato Delboni",
+    photo: require("../assets/RenP.png")
+  },
+  {
+    type: 'contact',
+    name: "Tatiana",
+    photo: require("../assets/kitty.jpeg")
+  },
+  {
+    type: 'contact',
+    name: "Lilian",
+    photo: require("../assets/doggy.JPG")
+  }
+]
 
-const ContactsMenu = () => {
+function ContactsMenu() {
   return (
     <View style={styles.container}>
-      {/* Contact Container  */}
-      <View style={styles.row}>
-        {/* Image */}
-        <View style={styles.starredIcon}>
-          <AntDesign name="star" size={30} color="#efefef" />
+      {/* Contact Container, take whole view inside loop/iteration  */}
+      {contactsMenuButtons.map((contact, index) =>
+        <View key={index} style={styles.row}>
+          {/* Image */}
+          {contact.type == "starred" ? (
+            <View style={styles.starredIcon}>
+              <AntDesign name="star" size={30} color="#efefef" />
+            </View>) :
+            (
+              <Image source={contact.photo} style={styles.image} />
+            )
+          }
+          {/* Text */}
+          <Text style={styles.text}>
+            Starred
+          </Text>
         </View>
-        {/* Text */}
-        <Text style={styles.text}>
-          Starred
-        </Text>
-      </View>
+      )}
     </View>
   );
-};
+}
 
 export default ContactsMenu;
 
@@ -29,12 +56,15 @@ const styles = StyleSheet.create({
   },
 
   text: {
-
+    color: "white",
+    paddingLeft: 15,
+    fontSize: 18
   },
 
   row: {
     flexDirection: "row",
-
+    marginTop: 20,
+    alignItems: "center"
   },
 
   starredIcon: {
@@ -43,6 +73,6 @@ const styles = StyleSheet.create({
     height: 55,
     justifyContent: "center",
     alignItems: "center",
-
+    borderRadius: 20
   }
 })
