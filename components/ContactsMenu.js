5470413931bda16fe27738651faf88bdb991c614
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-19 10:33:12 +0900

    added contact menu button

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 
 const ContactsMenu = () => {
   return (
-    <View>
-      <Text>ContactsMenu</Text>
+    <View style={styles.container}>
+      {/* Contact Container  */}
+      <View style={styles.row}>
+        {/* Image */}
+        <View style={styles.starredIcon}>
+          <AntDesign name="star" size={30} color="#efefef" />
+        </View>
+        {/* Text */}
+        <Text style={styles.text}>
+          Starred
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default ContactsMenu;
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+
+  text: {
+
+  },
+
+  row: {
+    flexDirection: "row",
+
+  },
+
+  starredIcon: {
+    backgroundColor: "#333333",
+    width: 55,
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+
+  }
+})
